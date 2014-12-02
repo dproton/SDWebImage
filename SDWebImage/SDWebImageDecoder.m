78bcf34a7c8ec096303faaf38dd531aa6525ef66
DungLe <dung.le@audiencemedia.com>
2014-12-02 13:43:06 +0700

    Fixed memory issue when download large image !

--- SDWebImage/SDWebImageDecoder.m
+++ SDWebImage/SDWebImageDecoder.m
@@ -9,64 +9,85 @@
  */
 
 #import "SDWebImageDecoder.h"
-
+#define MAX_IMAGE_SIZE 1000
 @implementation UIImage (ForceDecode)
-
++ (CGSize) originalSize:(UIImage*) image
+{
+    return  CGSizeMake(CGImageGetWidth(image.CGImage), CGImageGetHeight(image.CGImage));
+}
 + (UIImage *)decodedImageWithImage:(UIImage *)image {
+    return [self decodedImageWithImage:image maxSize:MAX_IMAGE_SIZE];
+}
++ (UIImage *)decodedImageWithImage:(UIImage *)image maxSize:(NSInteger) maxSize{
     if (image.images) {
         // Do not decode animated images
         return image;
     }
-
+    
     CGImageRef imageRef = image.CGImage;
     CGSize imageSize = CGSizeMake(CGImageGetWidth(imageRef), CGImageGetHeight(imageRef));
+    imageSize = [self checkMaxImageSize:imageSize maxSize:maxSize];
     CGRect imageRect = (CGRect){.origin = CGPointZero, .size = imageSize};
-
+    
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
     CGBitmapInfo bitmapInfo = CGImageGetBitmapInfo(imageRef);
-
+    
     int infoMask = (bitmapInfo & kCGBitmapAlphaInfoMask);
     BOOL anyNonAlpha = (infoMask == kCGImageAlphaNone ||
-            infoMask == kCGImageAlphaNoneSkipFirst ||
-            infoMask == kCGImageAlphaNoneSkipLast);
-
+                        infoMask == kCGImageAlphaNoneSkipFirst ||
+                        infoMask == kCGImageAlphaNoneSkipLast);
+    
     // CGBitmapContextCreate doesn't support kCGImageAlphaNone with RGB.
     // https://developer.apple.com/library/mac/#qa/qa1037/_index.html
     if (infoMask == kCGImageAlphaNone && CGColorSpaceGetNumberOfComponents(colorSpace) > 1) {
         // Unset the old alpha info.
         bitmapInfo &= ~kCGBitmapAlphaInfoMask;
-
+        
         // Set noneSkipFirst.
         bitmapInfo |= kCGImageAlphaNoneSkipFirst;
     }
-            // Some PNGs tell us they have alpha but only 3 components. Odd.
+    // Some PNGs tell us they have alpha but only 3 components. Odd.
     else if (!anyNonAlpha && CGColorSpaceGetNumberOfComponents(colorSpace) == 3) {
         // Unset the old alpha info.
         bitmapInfo &= ~kCGBitmapAlphaInfoMask;
         bitmapInfo |= kCGImageAlphaPremultipliedFirst;
     }
-
+    
     // It calculates the bytes-per-row based on the bitsPerComponent and width arguments.
     CGContextRef context = CGBitmapContextCreate(NULL,
-            imageSize.width,
-            imageSize.height,
-            CGImageGetBitsPerComponent(imageRef),
-            0,
-            colorSpace,
-            bitmapInfo);
+                                                 imageSize.width,
+                                                 imageSize.height,
+                                                 CGImageGetBitsPerComponent(imageRef),
+                                                 0,
+                                                 colorSpace,
+                                                 bitmapInfo);
     CGColorSpaceRelease(colorSpace);
-
+    
     // If failed, return undecompressed image
     if (!context) return image;
-
+    
     CGContextDrawImage(context, imageRect, imageRef);
     CGImageRef decompressedImageRef = CGBitmapContextCreateImage(context);
-
+    
     CGContextRelease(context);
-
+    
     UIImage *decompressedImage = [UIImage imageWithCGImage:decompressedImageRef scale:image.scale orientation:image.imageOrientation];
     CGImageRelease(decompressedImageRef);
     return decompressedImage;
 }
 
++ (CGSize) checkMaxImageSize:(CGSize) size maxSize:(NSInteger) max
+{
+    CGFloat whRatio = size.width / size.height;
+    if (size.height > max || size.width > max) {
+        if (size.height > size.width) {
+            size.width = max * whRatio;
+            size.height = max;
+        }else{
+            size.width = max ;
+            size.height = max / whRatio;
+        }
+    }
+    return size;
+}
 @end
